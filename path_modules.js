3cb2d64d474b5dfd8110926ca7319c2813a62095
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-08 22:37:59 +0530

    #9: Path Module in Node.js

---
+++ path_modules.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// school folder/students/data.txt
+
+const filePath = path.join("folder", "students", "data.txt");
+
+const parseData = path.parse(filePath);
+
+const resolvedPath = path.resolve(filePath);
+const extname = path.extname(filePath);
+const basename = path.basename(filePath);
+const dirname = path.dirname(filePath);
+const seprator = path.sep;
+
+// console.log(filePath);
+
+console.log({ parseData, resolvedPath, extname, basename, dirname, seprator });
