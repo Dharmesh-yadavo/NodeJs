a161f9b962f22a29822e95c01e43ddaf94648583
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-08 20:40:05 +0530

    #8: Named & Aggregate Exports in Node.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-// Import way
-const add = require("./math");
-console.log(add(2, 3));
+//! Import way
+// const add = require("./math");
+// const mult = require("./math");
+// This is not possible
+
+// const { add, mult} = require("./math");
+
+// const { add, mult, sub, div } = require("./math");
+
+// console.log(add(2, 3));
+// console.log(mult(2, 3));
+// console.log(sub(2, 3));
+// console.log(div(2, 3));
+
+// One more way
+
+const math = require("./math");
+
+console.log(math.add(2, 3));
+console.log(math.mult(2, 3));
+console.log(math.sub(2, 3));
+console.log(math.div(2, 3));
--- math.js
+++ math.js
@@ -1,4 +1,19 @@
 const add = (a, b) => a + b;
 
-//export way
-module.exports = add;
+const sub = (a, b) => a - b;
+
+const mult = (a, b) => a * b;
+
+const div = (a, b) => a / b;
+
+//! export way
+// module.exports = add;
+// module.exports = mult;
+// This is only possible for one fxn
+
+// for two you have to do differently
+// module.exports.add = add;
+// module.exports.mult = mult;
+
+// one more easy way
+module.exports = { add, mult, sub, div };
