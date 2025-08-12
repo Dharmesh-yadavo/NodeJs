a576295cceac7b50444274235b176ce7d41db0fa
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-12 16:52:50 +0530

    Node.js Project #1: Build a CLI Todo App Using Node.js

---
+++ mini_project/todo_list/app.js
@@ -0,0 +1,38 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const todos = [];
+
+const showMenu = () => {
+  console.log("1: Add a task");
+  console.log("2: View Task");
+  console.log("3: Exit");
+  rl.question("Choose an option: ", handleInput);
+};
+
+const handleInput = (option) => {
+  if (option === "1") {
+    rl.question("Enter the Task: ", (task) => {
+      todos.push(task);
+      console.log("Task added:", task);
+      showMenu();
+    });
+  } else if (option === "2") {
+    console.log("\nYour Todo Lists: ");
+    todos.forEach((curTask, index) => {
+      console.log(`${index + 1}. ${curTask}`);
+    });
+    showMenu();
+  } else if (option === "3") {
+    console.log("\nGood Bye !!! ");
+  } else {
+    console.log("Invalid Option. Please try again");
+    showMenu();
+  }
+};
+
+showMenu();
