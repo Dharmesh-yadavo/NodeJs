b8bfec9fc45231da1e08d99b5c079a6fc9716d61
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-16 23:06:29 +0530

    🔥 Serve HTML CSS & JavaScript Files in Express.js

--- express/app.js
+++ express/app.js
@@ -1,20 +1,43 @@
 import express from "express";
 import { PORT } from "./env.js";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World !</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello World !</h1>");
+// });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>Hello About Page !</h1>");
-});
+// app.get("/about", (req, res) => {
+//   res.send("<h1>Hello About Page !</h1>");
+// });
 
 // console.log(process);
 
 // const PORT = 3000;
 // const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  // these will not work in
+  // console.log(__dirname);
+  // console.log(__filename);
+  //* alternative of dirname:
+  // console.log(import.meta.dirname);
+  //* alternative of filename:
+  // console.log(import.meta.filename);
+  // const __filename = new URL(import.meta.url).pathname;
+  // console.log(__filename);
+  // res.send("Hi");
+
+  //! importing css
+  // app.use(express.static("public"));
+  //* static path
+  const staticPath = path.join(import.meta.dirname, "public");
+  app.use(express.static(staticPath));
+
+  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+  res.sendFile(homePagePath);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at: ${PORT}`);
