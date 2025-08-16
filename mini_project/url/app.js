9e29f6a1c72521df54e8ffdaed1e12309160042e
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-08-16 15:52:01 +0530

    url data going to backend

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -1,8 +1,10 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { createServer } from "http";
 import path from "path";
+import crypto from "crypto";
 
 const PORT = 3001;
+const DATA_FILE = path.join("data", "links.json");
 
 const serveFile = async (res, filePath, contentType) => {
   try {
@@ -10,12 +12,29 @@ const serveFile = async (res, filePath, contentType) => {
     res.writeHead(200, { "content-type": contentType });
     res.end(data);
   } catch (error) {
-    res.writeHead(404, { "Content-type": "text / plain" });
+    res.writeHead(404, { "Content-type": "text/plain" });
     res.end("404 page not found");
   }
 };
 
-const server = createServer((req, res) => {
+const loadLinks = async () => {
+  try {
+    const data = await readFile(DATA_FILE, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await writeFile(DATA_FILE, JSON.stringify({}));
+      return {};
+    }
+    throw error;
+  }
+};
+
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links, null, 2));
+};
+
+const server = createServer(async (req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
       //   try {
@@ -28,6 +47,35 @@ const server = createServer((req, res) => {
       //   }
       return serveFile(res, path.join("public", "index.html"), "text/html");
     }
+  }
+
+  if (req.method === "POST" && req.url === "/shorten") {
+    const links = await loadLinks();
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", async () => {
+      const { url, shortCode } = JSON.parse(body);
+
+      if (!url) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        return res.end("URL required");
+      }
+
+      const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+      if (links[finalShortCode]) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        return res.end("Short code already exists. Please choose another. ");
+      }
+
+      links[finalShortCode] = url;
+      await saveLinks(links);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end();
+    });
   }
 });
 
