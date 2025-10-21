06effe4e9b9c506ff4b220f6cb39a8908fdf41ca
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-10-21 22:15:22 +0530

    MVC ( model view controller ) in expressJs

--- express/url_shortener/router/shortener.routes.js
+++ express/url_shortener/router/shortener.routes.js
@@ -1,94 +1,33 @@
-import path from "path";
-import crypto from "crypto";
-import { readFile, writeFile } from "fs/promises";
 import { Router } from "express";
+import {
+  getUrlShortener,
+  postUrlShortener,
+  redirectToShortLink,
+} from "../controllers/postUrlShortener.controllers.js";
 
 const router = Router();
 
-const DATA_FILE = path.join("data", "links.json");
+router.get("/", getUrlShortener);
 
-const loadLinks = async () => {
-  try {
-    const data = await readFile(DATA_FILE, "utf-8");
-    return JSON.parse(data);
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      await writeFile(DATA_FILE, JSON.stringify({}));
-      return {};
-    }
-    throw error;
-  }
-};
+//! ejs
+// router.get("/report", (req, res) => {
+//   const student = [
+//     {
+//       name: "Dharmesh",
+//       grade: "12th",
+//       favoriteSubject: "Math",
+//     },
+//     { name: "Aarav", grade: "10th", favoriteSubject: "Mathematics" },
+//     { name: "Ishita", grade: "9th", favoriteSubject: "Science" },
+//     { name: "Rohan", grade: "8th", favoriteSubject: "History" },
+//     { name: "Meera", grade: "10th", favoriteSubject: "English" },
+//     { name: "Kabir", grade: "11th", favoriteSubject: "Physics" },
+//   ];
+//   res.render("report", { student });
+// });
 
-const saveLinks = async (links) => {
-  await writeFile(DATA_FILE, JSON.stringify(links, null, 2));
-};
+router.post("/", postUrlShortener);
 
-router.get("/", async (req, res) => {
-  try {
-    const file = await readFile(path.join("views", "index.html"));
-    const links = await loadLinks();
-    const content = file.toString().replaceAll(
-      "{{ shortened_urls }}",
-      Object.entries(links)
-        .map(
-          ([shortCode, url]) =>
-            `<li><a href = "/${shortCode}" target = "_blank"> ${req.host}/${shortCode} </a> - ${url}</li>`
-        )
-        .join("")
-    );
-    res.send(content);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Internal server error");
-  }
-});
-
-//ejs
-router.get("/report", (req, res) => {
-  const student = [
-    {
-      name: "Dharmesh",
-      grade: "12th",
-      favoriteSubject: "Math",
-    },
-    { name: "Aarav", grade: "10th", favoriteSubject: "Mathematics" },
-    { name: "Ishita", grade: "9th", favoriteSubject: "Science" },
-    { name: "Rohan", grade: "8th", favoriteSubject: "History" },
-    { name: "Meera", grade: "10th", favoriteSubject: "English" },
-    { name: "Kabir", grade: "11th", favoriteSubject: "Physics" },
-  ];
-  res.render("report", { student });
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const { url, shortCode } = req.body;
-    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-    const links = await loadLinks();
-    if (links[finalShortCode]) {
-      return res
-        .status(400)
-        .send("Short code already exists. Please choose another. ");
-    }
-    links[finalShortCode] = url;
-    await saveLinks(links);
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/:shortCode", async (req, res) => {
-  try {
-    const { shortCode } = req.params;
-    const links = await loadLinks();
-    if (!links[shortCode]) return res.status(404).send("404 error occured");
-    return res.redirect(links[shortCode]);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Internal server error");
-  }
-});
+router.get("/:shortCode", redirectToShortLink);
 
 export const shortenedRoutes = router;
